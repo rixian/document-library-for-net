35be7e849e90ec287e0e5931c6b1b365c2665f2a
Samuel McAravey <mcaravey@rixian.net>
2019-10-04 19:20:26 -0700

    Initial template commit

---
+++ src/Library.Tests/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+// Copyright (c) COMPANY-PLACEHOLDER. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE file in the project root for full license information.
+
+// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Unit Tests")]

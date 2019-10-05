aedb0db29c8e7bc272faac7ffbb37742d6adbffd
Samuel McAravey <mcaravey@rixian.net>
2019-10-04 19:21:39 -0700

    Expanded template for VendorHub.DocumentLibrary
    This expansion done by the (now removed) Expand-Template.ps1 script.

--- src/VendorHub.DocumentLibrary.Tests/CalculatorTests.cs
+++ src/VendorHub.DocumentLibrary.Tests/CalculatorTests.cs
@@ -1,8 +1,8 @@
-﻿// Copyright (c) COMPANY-PLACEHOLDER. All rights reserved.
+// Copyright (c) Rixian. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
-using Library;
+using VendorHub.DocumentLibrary;
 using Xunit;
 using Xunit.Abstractions;
 
--- src/VendorHub.DocumentLibrary/Calculator.cs
+++ src/VendorHub.DocumentLibrary/Calculator.cs
@@ -1,7 +1,7 @@
-﻿// Copyright (c) COMPANY-PLACEHOLDER. All rights reserved.
+// Copyright (c) Rixian. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Library
+namespace VendorHub.DocumentLibrary
 {
     using System;
 

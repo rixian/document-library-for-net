bdbcfa395386690f5e44f4186323846bbd2b40c3
Samuel McAravey <mcaravey@rixian.net>
2019-10-07 21:17:10 -0700

    Build fixes.

---
+++ src/VendorHub.DocumentLibrary/SearchResult`1.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Rixian. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENSE file in the project root for full license information.
+
+namespace VendorHub.DocumentLibrary
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using Newtonsoft.Json;
+    using Rixian.Drive.Common;
+
+    /// <summary>
+    /// Represents a search result.
+    /// </summary>
+    /// <typeparam name="T">The document type of the search result.</typeparam>
+    public class SearchResult<T>
+        where T : class
+    {
+        /// <summary>
+        /// Gets or sets the document found from the search.
+        /// </summary>
+        [JsonProperty("document", Required = Required.Always)]
+        public T? Document { get; set; }
+
+        /// <summary>
+        /// Gets or sets the search score.
+        /// </summary>
+        [JsonProperty("@search.score", Required = Required.Always)]
+        [Required(AllowEmptyStrings = true)]
+        public double? SearchScore { get; set; }
+
+        /// <summary>
+        /// Gets or sets any additional properties.
+        /// </summary>
+        [JsonExtensionData]
+#pragma warning disable CA2227 // Collection properties should be read only
+        public IDictionary<string, object> AdditionalProperties { get; set; } = new Dictionary<string, object>();
+#pragma warning restore CA2227 // Collection properties should be read only
+
+    }
+}

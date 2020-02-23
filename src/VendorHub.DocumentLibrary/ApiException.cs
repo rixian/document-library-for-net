b5ba3a6ecd4e8940aa3a6b78b3cab434693a60d9
Samuel McAravey <mcaravey@rixian.net>
2020-02-22 17:25:44 -0800

    NuGet updates and added missing classes.

---
+++ src/VendorHub.DocumentLibrary/ApiException.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Rixian. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENSE file in the project root for full license information.
+
+namespace VendorHub.DocumentLibrary
+{
+    using System;
+    using System.Runtime.Serialization;
+    using Newtonsoft.Json;
+    using Rixian.Extensions.Errors;
+
+    /// <summary>
+    /// Represents an error from an Api HTTP request.
+    /// </summary>
+    [Serializable]
+    public class ApiException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiException"/> class.
+        /// </summary>
+        public ApiException()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiException"/> class.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        public ApiException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiException"/> class.
+        /// </summary>
+        /// <param name="message">The error message.</param>
+        /// <param name="innerException">The error inner exception.</param>
+        public ApiException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApiException"/> class.
+        /// </summary>
+        /// <param name="info">The SerializationInfo for this exception.</param>
+        /// <param name="context">The StreamingContext for this exception.</param>
+        protected ApiException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of an ApiException using an ErrorBase object.
+        /// </summary>
+        /// <param name="error">The error to use for populating the exception.</param>
+        /// <returns>The initialized ApiException.</returns>
+        public static ApiException Create(ErrorBase error)
+        {
+            var errorMessage = JsonConvert.SerializeObject(error, Formatting.Indented);
+            return new ApiException(errorMessage);
+        }
+    }
+}
---
+++ src/VendorHub.DocumentLibrary/UnexpectedStatusCodeError.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Rixian. All rights reserved.
+// Licensed under the Apache License, Version 2.0 license. See LICENSE file in the project root for full license information.
+
+namespace VendorHub.DocumentLibrary
+{
+    using System;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Rixian.Extensions.Errors;
+
+    /// <summary>
+    /// Standard error for unexpected status codes returned from an HTTP request.
+    /// </summary>
+    public class UnexpectedStatusCodeError : ErrorBase
+    {
+        private UnexpectedStatusCodeError()
+        {
+        }
+
+        /// <summary>
+        /// Gets the content of the faulted response.
+        /// </summary>
+        public string? Content { get; private set; }
+
+        /// <summary>
+        /// Gets the server provided reason phrase of the faulted response.
+        /// </summary>
+        public string? ReasonPhrase { get; private set; }
+
+        /// <summary>
+        /// Gets the status code of the faulted response.
+        /// </summary>
+        public HttpStatusCode StatusCode { get; private set; }
+
+        /// <summary>
+        /// Creates a new instance of an UnexpectedStatusCodeError.
+        /// </summary>
+        /// <param name="response">The HttpResponseMessage to convert to an error.</param>
+        /// <param name="target">The target of the error.</param>
+        /// <returns>The new UnexpectedStatusCodeError.</returns>
+        public static async Task<UnexpectedStatusCodeError> CreateAsync(HttpResponseMessage response, string target)
+        {
+            if (response is null)
+            {
+                throw new ArgumentNullException(nameof(response));
+            }
+
+            var error = new UnexpectedStatusCodeError();
+            error.Code = "UnexpectedStatusCode";
+            error.Target = target;
+            error.Message = $"The HTTP status code of the response was not expected ({(int)response.StatusCode}).";
+
+            if (response.Content != null)
+            {
+                var stringContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                error.Content = stringContent;
+            }
+
+            error.ReasonPhrase = response.ReasonPhrase;
+            error.StatusCode = response.StatusCode;
+
+            return error;
+        }
+    }
+}
